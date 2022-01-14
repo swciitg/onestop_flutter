ca224ebfe64191a57c6b266821c3d126a6757a1c
Vignesh Rao <vigneshravirao@gmail.com>
2022-01-14 16:32:35 +0530

    Basic Auth complete

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:onestop_dev/stores/login_store.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   static String id = "/home";
@@ -14,8 +16,13 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Text('Logged In'),
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children:  [
+          Text('${context.read<LoginStore>().userData['name']}',textAlign: TextAlign.center,),
+          Text('${context.read<LoginStore>().userData['rollno']}',textAlign: TextAlign.center,),
+          ElevatedButton(onPressed: (){
+            context.read<LoginStore>().logOut(context);
+          }, child: const Text('Log Out')),
         ],
       ),
     );
