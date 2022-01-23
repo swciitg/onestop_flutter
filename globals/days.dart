04949b9594a271cc3ba8de78975201160813ff78
ss0313 <76594980+ss0313@users.noreply.github.com>
2022-01-23 23:44:30 +0530

    Delete globals directory

--- globals/days.dart
+++
@@ -1,9 +0,0 @@
-const Map<String, int> kWeekday = {
-  "Monday": 1,
-  "Tuesday": 2,
-  "Wednesday": 3,
-  "Thursday": 4,
-  "Friday": 5,
-  "Saturday": 6,
-  "Sunday": 7
-};
--- globals/mySpaces.dart
+++
@@ -1,8 +0,0 @@
-import 'package:onestop_dev/globals/sizeConfig.dart';
-
-class MySpaces {
-  static double get horizontalScreenPadding =>
-      (4.86 * SizeConfig.horizontalBlockSize!);
-  static double get listTileLeftPadding =>
-      (0.06 * (SizeConfig.screenWidth! - 2 * 10));
-}
