8443497e0c3f2cbff538368f8341907ba777f468
Aishani2001 <aishanimitra15102001@gmail.com>
2022-02-17 12:28:33 +0530

    Size issue fixed

--- lib/pages/foodtab.dart
+++ lib/pages/foodtab.dart
@@ -13,17 +13,17 @@ class FoodTab extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
         SizedBox(
-          height: 10,
+          height: 8,
         ),
         FoodSearchBar(),
         SizedBox(
-          height: 10,
+          height: 8,
         ),
         MessMenu(),
-        SizedBox(height: 10),
+        SizedBox(height: 8),
         FavoriteDishes(),
         SizedBox(
-          height: 10,
+          height: 8,
         ),
         OutletsFilter(),
         restaurant(),
@@ -40,7 +40,7 @@ class OutletsFilter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 70,
+      height: 40,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -101,7 +101,7 @@ class OutletsFilterTile extends StatelessWidget {
             padding: const EdgeInsets.all(12.0),
             child: FittedBox(
               child: ConstrainedBox(
-                constraints: BoxConstraints(minWidth: 40),
+                constraints: BoxConstraints(minWidth: 20),
                 child: Text(
                   filterText,
                   style: selected
@@ -296,7 +296,7 @@ class restaurant extends StatelessWidget {
         color: kBackground,
       ),
       child: Container(
-          height: 160,
+          height: 186,
           padding: const EdgeInsets.all(4),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -358,7 +358,7 @@ class FavoriteDishes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 232,
+      height: 180,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
         color: kHomeTile,
@@ -454,7 +454,7 @@ class favouriteFoodDetails extends StatelessWidget {
     return Column(
       children: [
         SizedBox(
-            height: 70,
+            height: 50,
             width: 67,
             child: ClipRRect(
               borderRadius: BorderRadius.circular(25),
