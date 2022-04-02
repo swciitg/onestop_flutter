fd2699142c33ee77a99285ddf97178c29e281628
pawaneet singh <52955316+Pawaneet56@users.noreply.github.com>
2022-04-02 22:07:46 +0530

    ip settings

---
+++ lib/pages/hosteldetails.dart
@@ -0,0 +1,7 @@
+class hostelDetails{
+   late String hostelName;
+   late String block;
+  late String floor;
+  late String roomNo;
+  hostelDetails(this.hostelName,this.block,this.floor,this.roomNo);
+}
