6429023bf27540c61cea1c663480d9c458360197
Kunalpal215 <kunalpal215@gmail.com>
2022-04-24 02:06:33 +0530

    some mess menu issues fixed

--- lib/pages/food/food_tab.dart
+++ lib/pages/food/food_tab.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -135,12 +136,21 @@ class OutletsFilter extends StatelessWidget {
 }
 
 class MessMenu extends StatelessWidget {
-  const MessMenu({
+  MessMenu({
     Key? key,
   }) : super(key: key);
 
+  StreamController mealController = StreamController();
+  StreamController<String> dayController = StreamController();
+  StreamController<String> hostelController = StreamController();
+
+  List<String> days = ["Sun", "Mon", "Tue","Wed","Thu","Fri","Sat"];
+  List<String> hostels = ["Brahmaputra", "Lohit", "Kameng", "Umiam", "Barak", "Manas", "Dihing", "Disang"];
+
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    print(screenWidth);
     return Container(
         height: 160,
         decoration: BoxDecoration(
@@ -150,19 +160,48 @@ class MessMenu extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(16),
           child: Row(children: [
-            Expanded(
-                flex: 1,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    MessMeal(mealName: "Breakfast"),
-                    MessMeal(mealName: "Lunch"),
-                    MessMeal(
-                      mealName: "Dinner",
-                      selected: true,
-                    ),
-                  ],
-                )),
+            StreamBuilder(
+              stream: mealController.stream,
+              builder: (context, mealSnapshot) {
+                return Expanded(
+                    flex: 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      // mainAxisSize: MainAxisSize.min,
+                      children: [
+                        GestureDetector(
+                          onTap : (){
+                            if(mealSnapshot.hasData == true && mealSnapshot.data=="Breakfast"){
+                              return;
+                            }
+                            mealController.sink.add("Breakfast");
+                          },
+                            child: MessMeal(mealName: "Breakfast",selected: (mealSnapshot.hasData == true && mealSnapshot.data=="Breakfast") ? true : false,)),
+                        GestureDetector(
+                            onTap : (){
+                              if(mealSnapshot.hasData == true && mealSnapshot.data=="Lunch"){
+                                return;
+                              }
+                              mealController.sink.add("Lunch");
+                            },
+                            child: MessMeal(mealName: "Lunch",selected: (mealSnapshot.hasData == true && mealSnapshot.data=="Lunch") ? true : false,)),
+                        GestureDetector(
+                          onTap: (){
+                            if(mealSnapshot.hasData == true && mealSnapshot.data=="Dinner"){
+                              return;
+                            }
+                            mealController.sink.add("Dinner");
+                          },
+                          child: MessMeal(
+                            mealName: "Dinner",
+                            selected: (mealSnapshot.hasData == true && mealSnapshot.data=="Dinner") ? true : false,
+                          ),
+                        ),
+                      ],
+                    ));
+              }
+            ),
             SizedBox(
               width: 16,
             ),
@@ -187,47 +226,56 @@ class MessMenu extends StatelessWidget {
                     Expanded(
                         flex: 2,
                         child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.fromLTRB(1, 0, 2, 0),
-                                child: PopupMenuButton<String>(
-                                  onSelected: (value) => print(value),
-                                  itemBuilder: (context) {
-                                    return ["Sun", "Mon", "Tue"]
-                                        .map(
-                                          (value) => PopupMenuItem(
-                                            value: value,
-                                            child: Text(value),
-                                          ),
-                                        )
-                                        .toList();
-                                  },
-                                  offset: Offset(1, 40),
-                                  child: Container(
-                                    padding: EdgeInsets.all(4.0),
-                                    decoration: BoxDecoration(
-                                        color: kGrey2,
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(20))),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceAround,
-                                      children: [
-                                        Padding(
-                                          padding: const EdgeInsets.fromLTRB(
-                                              4, 0, 0, 2),
-                                          child: Text('Mon',
-                                              style: MyFonts.medium
-                                                  .setColor(lBlue)
-                                                  .size(12)),
-                                        ),
-                                        Icon(
-                                          Icons.keyboard_arrow_down_outlined,
-                                          color: lBlue,
-                                        ),
-                                      ],
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(1, 0, 2, 0),
+                              child: PopupMenuButton<String>(
+                                onSelected: (value) => print(value),
+                                itemBuilder: (context) {
+                                  return days
+                                      .map(
+                                        (value) => PopupMenuItem(
+                                      onTap: (){
+                                        dayController.sink.add(value);
+                                      },
+                                      value: value,
+                                      child: Text(value),
                                     ),
+                                  )
+                                      .toList();
+                                },
+                                offset: Offset(1, 40),
+                                child: Container(
+                                  padding: EdgeInsets.all(4.0),
+                                  decoration: BoxDecoration(
+                                      color: kGrey2,
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(20))),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.fromLTRB(
+                                            4, 0, 4, 2),
+                                        child: StreamBuilder<String>(
+                                            stream: dayController.stream,
+                                            builder: (context, daySnapshot) {
+                                              return Text(daySnapshot.hasData==true ? daySnapshot.data! : "Mon",
+                                                  style: MyFonts.medium
+                                                      .setColor(lBlue)
+                                                      .size(screenWidth<=380 ? 10 : 13));
+                                            }
+                                        ),
+                                      ),
+                                      Icon(
+                                        Icons.keyboard_arrow_down_outlined,
+                                        color: lBlue,
+                                        size:  screenWidth<=380 ? 15: 20,
+                                      ),
+                                    ],
                                   ),
                                 ),
                               ),
@@ -235,49 +283,56 @@ class MessMenu extends StatelessWidget {
                             // SizedBox(
                             //   width: 8,
                             // ),
-                            Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.fromLTRB(2, 0, 1, 0),
-                                child: PopupMenuButton<String>(
-                                  onSelected: (value) => print(value),
-                                  itemBuilder: (context) {
-                                    return ["Brahma", "Lohit", "Kameng"]
-                                        .map(
-                                          (value) => PopupMenuItem(
-                                            value: value,
-                                            child: Text(
-                                              value,
-                                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(2, 0, 1, 0),
+                              child: PopupMenuButton<String>(
+                                onSelected: (value) => print(value),
+                                itemBuilder: (context) {
+                                  return hostels
+                                      .map(
+                                        (value) => PopupMenuItem(
+                                          onTap: (){
+                                            hostelController.sink.add(value);
+                                          },
+                                          value: value,
+                                          child: Text(
+                                            value,
                                           ),
-                                        )
-                                        .toList();
-                                  },
-                                  offset: Offset(1, 40),
-                                  child: Container(
-                                    padding: EdgeInsets.all(4.0),
-                                    decoration: BoxDecoration(
-                                        border: Border.all(color: lBlue),
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(20))),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceAround,
-                                      children: [
-                                        Padding(
-                                          padding: const EdgeInsets.fromLTRB(
-                                              4, 4, 0, 4),
-                                          child: Text('Subansiri',
-                                              style: MyFonts.medium
-                                                  .setColor(lBlue)
-                                                  .size(12)),
                                         ),
-                                        Icon(
-                                          Icons.keyboard_arrow_down_outlined,
-                                          color: lBlue,
-                                          size: 20,
+                                      )
+                                      .toList();
+                                },
+                                offset: Offset(1, 40),
+                                child: Container(
+                                  padding: EdgeInsets.all(4.0),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: lBlue),
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(20))),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.fromLTRB(
+                                            4, 4, 4, 4),
+                                        child: StreamBuilder<String>(
+                                          stream: hostelController.stream,
+                                          builder: (context, hostelSnapshot) {
+                                            return Text(hostelSnapshot.hasData == true ? hostelSnapshot.data! : "Brahmaputra",
+                                                style: MyFonts.medium
+                                                    .setColor(lBlue)
+                                                    .size(screenWidth<=380 ? 9 : 13));
+                                          }
                                         ),
-                                      ],
-                                    ),
+                                      ),
+                                      Icon(
+                                        Icons.keyboard_arrow_down_outlined,
+                                        color: lBlue,
+                                        size: screenWidth<=380 ? 15: 20,
+                                      ),
+                                    ],
                                   ),
                                 ),
                               ),
@@ -302,26 +357,22 @@ class MessMeal extends StatelessWidget {
   final bool selected;
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(3.0),
-        child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(150),
-              color: selected ? lBlue2 : lGrey,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: FittedBox(
-                child: Text(
-                  mealName,
-                  style: selected
-                      ? MyFonts.medium.size(23).setColor(kBlueGrey)
-                      : MyFonts.medium.size(23).setColor(lBlue),
-                ),
-              ),
-            )),
-      ),
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 3),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 5,vertical: 6),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(150),
+            color: selected ? lBlue2 : lGrey,
+          ),
+          alignment: Alignment.center,
+          child: Text(
+            mealName,
+            style: selected
+                ? MyFonts.medium.size(screenWidth<=380 ? 13 : 17).setColor(kBlueGrey)
+                : MyFonts.medium.size(screenWidth<=380 ? 13 : 17).setColor(lBlue),
+          ),),
     );
   }
 }
