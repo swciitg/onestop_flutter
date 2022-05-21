4e2733e7e500d26d24ec2db7579f72da57cce657
Hareesh nandigrama <85237630+Hareesh-Nandigrama@users.noreply.github.com>
2022-05-21 13:56:21 +0530

    Update contact.dart

--- lib/pages/contact.dart
+++ lib/pages/contact.dart
@@ -31,93 +31,127 @@ class _ContactPageState extends State<ContactPage> {
           ],
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
               padding: const EdgeInsets.fromLTRB(8,14,8,14),
               child: ContactSearchBar(),
             ),
-            Row(
-              children: [
-                Expanded(
-                  flex: 16,
-                  child: Container(),
-                ),
-                Expanded(
-                  flex: 106,
-                  child: Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: lGrey,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.warning, color: kGrey8,),
-                        Text('Emergency', style: MyFonts.medium.size(10).setColor(kWhite),),
-                      ],
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0,0,10),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 16,
+                    child: Container(),
+                  ),
+                  Expanded(
+                    flex: 106,
+                    child: Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: lGrey,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.warning, color: kGrey8,),
+                          Text('Emergency', style: MyFonts.medium.size(10).setColor(kWhite),),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  flex: 5,
-                  child: Container(),
-                ),
-                Expanded(
-                  flex: 106,
-                  child: Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: lGrey,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.directions_bus, color: kGrey8,),
-                        Text('Transport', style: MyFonts.medium.size(10).setColor(kWhite),),
-                      ],
+                  Expanded(
+                    flex: 5,
+                    child: Container(),
+                  ),
+                  Expanded(
+                    flex: 106,
+                    child: Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: lGrey,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.directions_bus, color: kGrey8,),
+                          Text('Transport', style: MyFonts.medium.size(10).setColor(kWhite),),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  flex: 5,
-                  child: Container(),
-                ),
-                Expanded(
-                  flex: 106,
-                  child: Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: lGrey,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.group, color: kGrey8,),
-                        Text('Gymkhana', style: MyFonts.medium.size(10).setColor(kWhite),),
-                      ],
+                  Expanded(
+                    flex: 5,
+                    child: Container(),
+                  ),
+                  Expanded(
+                    flex: 106,
+                    child: Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: lGrey,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.group, color: kGrey8,),
+                          Text('Gymkhana', style: MyFonts.medium.size(10).setColor(kWhite),),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  flex: 16,
-                  child: Container(),
-                ),
-              ],
+                  Expanded(
+                    flex: 16,
+                    child: Container(),
+                  ),
+                ],
+              ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(3,8,3,8),
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 6),
+              child: Row(
+                children: [
+                  Icon(IconData(0xe5f9, fontFamily: 'MaterialIcons'), color: kGrey8, size: 15,),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Text('Starred', style: MyFonts.regular.setColor(kGrey8),),
+                  ),
+                  Expanded(
+                    child: Container(),
+                  ),
+                ],
+              ),
+            ),
+            SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      starredContact('Gensec SWC'),
+                      starredContact('Admin, Finance'),
+                      starredContact('Gensec SWC'),
+                      starredContact('Admin, Finance'),
+                      starredContact('Gensec SWC'),
+                      starredContact('Admin, Finance'),
+
+                    ]
+                )
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(3,0,3,0),
               child: ListTile(
                 leading: CircleAvatar(
-                  radius: 20,
+                  radius: 15,
                   backgroundImage: NetworkImage('https://images.wallpapersden.com/image/wxl-loki-marvel-comics-show_78234.jpg'),
                 ),
                 title: Text('My Profile', style: MyFonts.bold.size(15).setColor(kWhite),),
               ),
             ),
-
             Expanded(
               child: AlphabetScrollView(
                 list: list.map((e) => AlphaModel(e)).toList(),
@@ -165,7 +199,6 @@ class _ContactPageState extends State<ContactPage> {
                 },
               ),
             )
-
 
           ],
         )
@@ -427,3 +460,29 @@ List<Map<String, dynamic>> Contacts10 = [
     'number': 32789108323,
   },
 ];
+
+starredContact(String contact)
+{
+  int size = contact.length;
+  return TextButton(
+    child: ClipRRect(
+      borderRadius: BorderRadius.all(
+        Radius.circular(40),
+      ),
+      child: Container(
+        height: 32,
+        width: 10*size+25,
+        color: lGrey,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              contact,
+              style: MyFonts.regular.setColor(kWhite),
+            ),
+          ],
+        ),
+      ),
+    ), onPressed: (){},);
+}
