5ee3390f3dba7a92b261f6bb4bc4d15c1e8e8c35
Hareesh nandigrama <85237630+Hareesh-Nandigrama@users.noreply.github.com>
2022-06-02 10:30:52 +0530

    Create contact_display.dart

---
+++ lib/widgets/contact/contact_display.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:onestop_dev/globals/my_colors.dart';
+import 'package:onestop_dev/globals/my_fonts.dart';
+
+class ContactText extends StatelessWidget {
+  final String text;
+  final align;
+  const ContactText({Key? key, required this.text, this.align}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 8.0),
+        child: Container(
+          alignment: align,
+          child: Text(
+            text,
+            style: MyFonts.regular.size(14).setColor(lBlue2),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ContactTextHeader extends StatelessWidget {
+  final text;
+  final width;
+  final align;
+  const ContactTextHeader({Key? key, this.text, this.width, this.align}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0),
+      child: Container(
+        alignment: align,
+        width: width,
+        child: Text(
+          text,
+          style: MyFonts.medium.size(12).setColor(kGrey11),
+        ),
+      ),
+    );
+  }
+}
