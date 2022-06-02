4bd71b62d3fc051dc52a76b925fd1a7cb2b6d278
Hareesh nandigrama <85237630+Hareesh-Nandigrama@users.noreply.github.com>
2022-06-02 10:38:20 +0530

    Refactored contact.dart

--- lib/pages/contact/contact.dart
+++ lib/pages/contact/contact.dart
@@ -1,14 +1,13 @@
 import 'dart:convert';
-
 import 'package:alphabet_scroll_view/alphabet_scroll_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-import '../../globals/my_colors.dart';
-import '../../globals/my_fonts.dart';
-import '../../models/contact_model.dart';
+import 'package:onestop_dev/globals/my_colors.dart';
+import 'package:onestop_dev/globals/my_fonts.dart';
+import 'package:onestop_dev/models/contact_model.dart';
 import 'contact_detail.dart';
-
+import 'package:onestop_dev/widgets/contact/contact_search_bar.dart';
+import 'package:onestop_dev/functions/contact/starred_contact.dart';
 
 Map<String, ContactModel> people= {};
 
@@ -64,10 +63,7 @@ class _ContactPageState extends State<ContactPage> {
               padding: const EdgeInsets.fromLTRB(0, 0,0,10),
               child: Row(
                 children: [
-                  Expanded(
-                    flex: 16,
-                    child: Container(),
-                  ),
+                  Expanded(flex: 16, child: Container(),),
                   Expanded(
                     flex: 106,
                     child: Container(
@@ -85,10 +81,7 @@ class _ContactPageState extends State<ContactPage> {
                       ),
                     ),
                   ),
-                  Expanded(
-                    flex: 5,
-                    child: Container(),
-                  ),
+                  Expanded(flex: 5, child: Container(),),
                   Expanded(
                     flex: 106,
                     child: Container(
@@ -106,10 +99,7 @@ class _ContactPageState extends State<ContactPage> {
                       ),
                     ),
                   ),
-                  Expanded(
-                    flex: 5,
-                    child: Container(),
-                  ),
+                  Expanded(flex: 5, child: Container(),),
                   Expanded(
                     flex: 106,
                     child: Container(
@@ -127,10 +117,7 @@ class _ContactPageState extends State<ContactPage> {
                       ),
                     ),
                   ),
-                  Expanded(
-                    flex: 16,
-                    child: Container(),
-                  ),
+                  Expanded(flex: 16, child: Container(),),
                 ],
               ),
             ),
@@ -143,9 +130,7 @@ class _ContactPageState extends State<ContactPage> {
                     padding: const EdgeInsets.only(left: 8),
                     child: Text('Starred', style: MyFonts.regular.setColor(kGrey8),),
                   ),
-                  Expanded(
-                    child: Container(),
-                  ),
+                  Expanded(child: Container(),),
                 ],
               ),
             ),
@@ -161,7 +146,6 @@ class _ContactPageState extends State<ContactPage> {
                       starredContact('Admin, Finance'),
                       starredContact('Gensec SWC'),
                       starredContact('Admin, Finance'),
-
                     ]
                 )
             ),
@@ -211,167 +195,4 @@ class _ContactPageState extends State<ContactPage> {
   }
 }
 
-class ContactSearchBar extends StatelessWidget {
 
-  ContactSearchBar({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 50,
-      child: GestureDetector(
-        onTap: (){
-          showSearch(context: context, delegate: CitySearch());
-        },
-        child: TextField(
-          enabled: false,
-          textAlignVertical: TextAlignVertical.center,
-          style: MyFonts.medium.setColor(kWhite),
-          decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(100),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(100.0),
-                borderSide: BorderSide(color: kBlueGrey, width: 1),
-              ),
-              filled: true,
-              prefixIcon: Icon(
-                Icons.search,
-                color: kWhite,
-              ),
-              hintStyle: MyFonts.medium.size(13).setColor(kGrey2),
-              hintText: "Search keyword (name, position etc)",
-              contentPadding: EdgeInsets.zero,
-              fillColor: kBlueGrey),
-        ),
-      ),
-    );
-  }
-}
-
-class CitySearch extends SearchDelegate<String> {
-  final cities = people.keys.toList();
-  var x;
-
-  @override
-  String get searchFieldLabel => 'Search keyword (name, position etc)';
-
-  @override
-  ThemeData appBarTheme(BuildContext context) {
-    return ThemeData(
-      scaffoldBackgroundColor: Color.fromRGBO(27, 27, 29, 1),
-      hintColor: kGrey2,
-      appBarTheme: AppBarTheme(
-        color: kBlueGrey, toolbarTextStyle: TextTheme(
-          headline6: TextStyle( // headline 6 affects the query text
-              color: kGrey2,
-              fontSize: 12.0,
-              fontWeight: FontWeight.bold)).bodyText2,
-      ),
-    );
-  }
-
-  @override
-  List<Widget> buildActions(BuildContext context) => [
-    IconButton(
-      icon: Icon(Icons.clear, color: kWhite,),
-      onPressed: () {
-        if (query.isEmpty) {
-          close(context, '');
-        } else {
-          query = '';
-          showSuggestions(context);
-        }
-      },
-    )
-  ];
-
-  @override
-  Widget buildLeading(BuildContext context) => IconButton(
-    icon: Icon(Icons.arrow_back, color: kWhite,),
-    onPressed: () => close(context, ''),
-  );
-
-
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    final suggestions = cities.where((city) {
-      final cityLower = city.toLowerCase();
-      final queryLower = query.toLowerCase();
-      return cityLower.contains(queryLower);
-    }).toList();
-    x = suggestions;
-    return buildSuggestionsSuccess(suggestions);
-  }
-
-  @override
-  Widget buildResults(BuildContext context) => buildSuggestionsSuccess(x);
-
-
-  Widget buildSuggestionsSuccess(List<String> suggestions) => ListView.builder(
-    itemCount: suggestions.length,
-    itemBuilder: (context, index) {
-      final suggestion = suggestions[index];
-      //final queryText = suggestion.substring(0, query.length);
-      //final remainingText = suggestion.substring(query.length);
-
-      return ListTile(
-        onTap: () {
-          query = suggestion;
-
-          // 1. Show Results
-          //showResults(context);
-
-          // 2. Close Search & Return Result
-          close(context, suggestion);
-
-          // 3. Navigate to Result Page
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (BuildContext context) => Contacts2(title: 'Campus',contact: people[query],),
-            ),
-          );
-        },
-        leading: Icon(Icons.people, color: kWhite,),
-        // title: Text(suggestion),
-        title: RichText(
-          text: TextSpan(
-            text: suggestion,
-            style: MyFonts.regular.size(15).setColor(kWhite),
-          ),
-        ),
-      );
-    },
-  );
-}
-
-starredContact(String contact)
-{
-  int size = contact.length;
-  return TextButton(
-    child: ClipRRect(
-      borderRadius: BorderRadius.all(
-        Radius.circular(40),
-      ),
-      child: Container(
-        height: 32,
-        width: 10*size+25,
-        color: lGrey,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              contact,
-              style: MyFonts.regular.setColor(kWhite),
-            ),
-          ],
-        ),
-      ),
-    ), onPressed: (){},);
-}
