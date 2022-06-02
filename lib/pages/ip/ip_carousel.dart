08b686284f1192070dc511e57d57e06329294d8a
Hareesh nandigrama <85237630+Hareesh-Nandigrama@users.noreply.github.com>
2022-06-02 10:02:51 +0530

    Refactor ip_carousel

--- lib/pages/ip/ip_carousel.dart
+++ lib/pages/ip/ip_carousel.dart
@@ -6,6 +6,17 @@ import 'package:onestop_dev/functions/ip/ip_decoration.dart';
 import 'package:onestop_dev/globals/my_colors.dart';
 import 'package:onestop_dev/globals/my_fonts.dart';
 
+List<String> textdata = [
+  'Open Start-> Control Panel -> Network and Internet-> Network and Sharing Center ',
+  "Click on 'Manage wireless networks'",
+  'Right click on \'Local area connection\' and then click on properties',
+  'Uncheck \'Internet Protocol Version 6 (TCP/IPv6)\' and double click \'Internet Protocol Version 4 (TCP/IPv4)\'',
+  'Select \'Use the following IP address\' and \'Use the following DNS server addresses\' Modify the DNS address as given below\nprimary DNS: 172.171.1.1\nsecondary DNS: 172.17.1.2',
+  'Enter the following details to get your IP address',
+  '',
+  "Make sure the connection is no-proxy/direct connection. Open any website in your browser. It will show a captive portal asking your IITG login credentials.Login to the portal and start accessing internet using the same.If you have a problem while redirecting to login page, then use this link given below in your pc browser https://agnigarh.iitg.ac.in:1442/login?"
+];
+
 bool fg = true;
 
 class RouterPage extends StatefulWidget {
@@ -24,6 +35,21 @@ class _RouterPageState extends State<RouterPage> {
   hostelDetails hostel =
   hostelDetails("nothing", "--", "--=-=-===", "something");
   String dropdownValue = "Select Hostel";
+  List<String> spinnerItems = [
+    'Select Hostel',
+    'Barak',
+    'Umiam',
+    'Brahmaputra',
+    'Manas',
+    'Dihing',
+    'Dibang',
+    'Married Scholars',
+    'Siang',
+    'Dhansiri',
+    'Subhansiri',
+    'Kapili',
+    'Kameng'
+  ];
   final _keyform = GlobalKey<FormState>();
 
   @override
@@ -213,7 +239,3 @@ class _RouterPageState extends State<RouterPage> {
     );
   }
 }
-
-
-
-
