6b512f2fe96e2272143aa0c7f44eeaa32a4a6da7
Hareesh nandigrama <85237630+Hareesh-Nandigrama@users.noreply.github.com>
2022-06-02 10:36:39 +0530

    Create starred_contact.dart

---
+++ lib/functions/contact/starred_contact.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:onestop_dev/globals/my_colors.dart';
+import 'package:onestop_dev/globals/my_fonts.dart';
+
+starredContact(String contact)
+{
+  int size = contact.length;
+  return TextButton(
+    child: ClipRRect(
+      borderRadius: BorderRadius.all(
+        Radius.circular(40),
+      ),
+      child: Container(
+        height: 32,
+        width: 10*size+25,
+        color: lGrey,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              contact,
+              style: MyFonts.regular.setColor(kWhite),
+            ),
+          ],
+        ),
+      ),
+    ), onPressed: (){},);
+}
