270c6016918d18c9bafbe1f0c67233df73022f53
babayaga2002 <csidharth@iitg.ac.in>
2022-06-09 16:26:49 +0530

    Circular progress Indicator on TimeTable

--- lib/pages/home/home_tab.dart
+++ lib/pages/home/home_tab.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:onestop_dev/globals.dart';
 import 'package:onestop_dev/pages/timetable/ApiCallingTimetable.dart';
-import 'package:onestop_dev/pages/timetable/timetable.dart';
 import 'package:onestop_dev/stores/login_store.dart';
 import 'package:onestop_dev/widgets/home/date_course.dart';
 import 'package:onestop_dev/widgets/home/quick_links.dart';
@@ -14,7 +13,6 @@ import '../../functions/timetable/Functions.dart';
 double lat = userlat;
 double long = userlong;
 
-
 class HomeTab extends StatefulWidget {
   const HomeTab({Key? key}) : super(key: key);
 
@@ -24,13 +22,15 @@ class HomeTab extends StatefulWidget {
 
 class _HomeTabState extends State<HomeTab> {
   int selectedIndex = 0;
-  String sel='';
+  String sel = '';
   Future<Time>? timetable;
+
   void initState() {
     timetable = ApiCalling().getTimeTable(
         roll: context.read<LoginStore>().userData["rollno"] ?? "200101095");
     super.initState();
   }
+
   void rebuildParent(int newSelectedIndex) {
     print('Reloaded');
     setState(() {
@@ -40,54 +40,56 @@ class _HomeTabState extends State<HomeTab> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Time>(
-      future: timetable,
-      builder: (BuildContext context, AsyncSnapshot<Time> snapshot) {
-        if (snapshot.hasData) {
-          List<Map<int, List<List<String>>>> data= ApiCalling().addWidgets(data: snapshot.data!);
-          return SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 10,
-                ),
-                MapBox(
-                  lat: (lat != 0) ? lat : null,
-                  long: (long != 0) ? long : null,
-                  selectedIndex: selectedIndex,
-                  rebuildParent: rebuildParent,
-                  istravel: true,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                DateCourse(
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          MapBox(
+            lat: (lat != 0) ? lat : null,
+            long: (long != 0) ? long : null,
+            selectedIndex: selectedIndex,
+            rebuildParent: rebuildParent,
+            istravel: true,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          FutureBuilder<Time>(
+            future: timetable,
+            builder: (BuildContext context, AsyncSnapshot<Time> snapshot) {
+              if (snapshot.hasData) {
+                List<Map<int, List<List<String>>>> data =
+                    ApiCalling().addWidgets(data: snapshot.data!);
+                sel=determiningSel();
+                return DateCourse(
                   sel: sel,
                   data: data,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                QuickLinks(),
-                SizedBox(
-                  height: 10,
-                ),
-                // Services(),
-                // SizedBox(
-                //   height: 10,
-                // ),
-              ],
-            ),
-          );
-        } else if (snapshot.hasError) {
-          Future.delayed(Duration.zero, () => reload(context));
-          return SizedBox();
-        } else
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-      },
+                );
+              } else if (snapshot.hasError) {
+                Future.delayed(Duration.zero, () => reload(context));
+                return SizedBox();
+              } else
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+            },
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          QuickLinks(),
+          SizedBox(
+            height: 10,
+          ),
+          // Services(),
+          // SizedBox(
+          //   height: 10,
+          // ),
+        ],
+      ),
     );
   }
 }
