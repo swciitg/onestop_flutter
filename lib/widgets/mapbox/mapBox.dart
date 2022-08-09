1cb67e3a4049819c59c32c2b420d19da4e3fe77f
Vignesh Rao <vigneshravirao@gmail.com>
2022-08-10 02:59:43 +0530

    Dark Mode Map

--- lib/widgets/mapbox/mapBox.dart
+++ lib/widgets/mapbox/mapBox.dart
@@ -48,24 +48,30 @@ class _MapBoxState extends State<MapBox> {
                 future: mapbox_store.getLocation(),
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
-                    return Container(
-                      height: 365,
-                      width: double.infinity,
-                      child: GoogleMap(
-                        onMapCreated: (mapcontroller){
-                          controller=mapcontroller;
-                          mapbox_store.mapController=mapcontroller;
-                        },
-                        initialCameraPosition: CameraPosition(
-                            target: snapshot.data as LatLng, zoom: 15),
-                        markers: mapbox_store.markers.toSet(),
-                        // polylines: poly.toSet(),
-                        myLocationEnabled: true,
-                        myLocationButtonEnabled: false,
-                        compassEnabled: true,
-                        trafficEnabled: true,
-                        zoomControlsEnabled: false,
-                      ),
+                    return Observer(
+                      builder: (context) {
+                        print("BUilder rebuild");
+                        return Container(
+                          height: 365,
+                          width: double.infinity,
+                          child: GoogleMap(
+                            onMapCreated: (mapcontroller){
+                              controller=mapcontroller;
+                              controller.setMapStyle(mapStyleString);
+                              mapbox_store.mapController=mapcontroller;
+                            },
+                            initialCameraPosition: CameraPosition(
+                                target: snapshot.data as LatLng, zoom: 15),
+                            markers: mapbox_store.markers.toSet(),
+                            // polylines: poly.toSet(),
+                            myLocationEnabled: true,
+                            myLocationButtonEnabled: false,
+                            compassEnabled: true,
+                            trafficEnabled: true,
+                            zoomControlsEnabled: false,
+                          ),
+                        );
+                      }
                     );
                   } else {
                     return ListShimmer(
@@ -335,4 +341,238 @@ class _MapBoxState extends State<MapBox> {
     );
   }
 }
+
+String mapStyleString = """[
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#1d2c4d"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#8ec3b9"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1a3646"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.country",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#4b6878"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#64779e"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.province",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#4b6878"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.man_made",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#334e87"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.natural",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#023e58"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#283d6a"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#6f9ba5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1d2c4d"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#023e58"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#3C7680"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#304a7d"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#98a5be"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1d2c4d"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#2c6675"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#255763"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#b0d5ce"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#023e58"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#98a5be"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1d2c4d"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#283d6a"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#3a4762"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#0e1626"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#4e6d70"
+      }
+    ]
+  }
+]""";
 
