29abc8be1cd1f34c4949fd0c94aa05fa85d8c5a7
Hareesh-Nandigrama <85237630+Hareesh-Nandigrama@users.noreply.github.com>
2022-08-09 22:20:22 +0530

    Update login_webview.dart
    Merge conflict resolved

--- lib/widgets/login/login_webview.dart
+++ lib/widgets/login/login_webview.dart
@@ -38,66 +38,6 @@ class LoginWebView extends StatelessWidget {
           var userInfoString = await controller.runJavascriptReturningResult(
               "document.querySelector('#userInfo').innerText");
           print(userInfoString);
-          if(userInfoString=="ERROR OCCURED") return;
-          var userInfo = {};
-
-          List<String> values = userInfoString.split("/");
-          userInfo["displayName"] = values[0].substring(1);
-          userInfo["mail"] = values[1];
-          userInfo["surname"] = values[2];
-          userInfo["id"] = values[3].split('"')[0];
-          print(userInfo);
-          SharedPreferences user = await SharedPreferences.getInstance();
-          context.read<LoginStore>().saveToPreferences(user, userInfo);
-          context.read<LoginStore>().saveToUserData(user);
-          Navigator.of(context)
-              .pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false);
-        }
-      },
-    );
-  }
-}
-=======
-import 'dart:async';
-
-import 'package:flutter/material.dart';
-import 'package:onestop_dev/stores/login_store.dart';
-import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-
-class LoginWebView extends StatelessWidget {
-  const LoginWebView({
-    Key? key,
-    required Completer<WebViewController> controller,
-  })  : _controller = controller,
-        super(key: key);
-
-  final Completer<WebViewController> _controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return WebView(
-      initialUrl: "https://swc.iitg.ac.in/onestopapi/auth/microsoft",
-      javascriptMode: JavascriptMode.unrestricted,
-      onWebViewCreated: (controller) {
-        _controller.complete(controller);
-      },
-      onPageFinished: (url) async {
-        if (url.startsWith(
-            "https://swc.iitg.ac.in/onestopapi/auth/microsoft/redirect?code")) {
-          WebViewController controller = await _controller.future;
-          var checkString = await controller.runJavascriptReturningResult(
-              "document.querySelector('h1').innerText");
-          print(checkString);
-          await controller
-              .runJavascriptReturningResult(
-                  "document.querySelector('#userInfo').innerText")
-              .then((value) => print(value))
-              .catchError((err) => print(err));
-          var userInfoString = await controller.runJavascriptReturningResult(
-              "document.querySelector('#userInfo').innerText");
-          print(userInfoString);
           var userInfo = {};
 
           List<String> values = userInfoString.replaceAll('"', '').split("/");
