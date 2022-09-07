d0952f6b58904bb3a5bf94059faf2136b87b7828
Kunalpal215 <kunalpal215@gmail.com>
2022-09-07 11:42:01 +0530

    shifted APIs to v2

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,13 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let dartDefinesString = Bundle.main.infoDictionary!["DART_DEFINES"] as! String
+    var dartDefinesDictionary = [String:String]()
+    for definedValue in dartDefinesString.components(separatedBy: ",") {
+        let decoded = String(data: Data(base64Encoded: definedValue)!, encoding: .utf8)!
+        let values = decoded.components(separatedBy: "=")
+        dartDefinesDictionary[values[0]] = values[1]
+    }
     GMSServices.provideAPIKey(Bundle.main.object(forInfoDictionaryKey: "GMAP_API_KEY") as? String ?? "")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
