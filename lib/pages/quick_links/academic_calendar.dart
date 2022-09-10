1ebd5566e33d1bc44bf5c10dd3ce2410f189e806
Vignesh Rao <vigneshravirao@gmail.com>
2022-09-11 01:52:13 +0530

    Add progress indicator to quick links

--- lib/pages/quick_links/academic_calendar.dart
+++ lib/pages/quick_links/academic_calendar.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:onestop_dev/globals/my_colors.dart';
+import 'package:onestop_dev/pages/lost_found/lnf_home.dart';
 import 'package:onestop_dev/widgets/ui/appbar.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -19,6 +21,9 @@ class _AcademicCalendar extends State<AcademicCalendar> {
   final Completer<WebViewController> _controller =
       Completer<WebViewController>();
 
+  double loadProgress = 0;
+  bool error = false;
+
   @override
   void initState() {
     super.initState();
@@ -32,20 +37,41 @@ class _AcademicCalendar extends State<AcademicCalendar> {
     return Scaffold(
       appBar: appBar(context, displayIcon: false),
       body: SafeArea(
-        child: WebView(
-          initialUrl: widget.url,
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (controller) {
-            _controller.complete(controller);
-          },
-          onWebResourceError: (context) {},
-          navigationDelegate: (NavigationRequest request) {
-            if (request.url.contains(widget.url)) {
-              // Don't allow user to navigate to other pages
-              return NavigationDecision.navigate;
-            }
-            return NavigationDecision.prevent;
-          },
+        child: Column(
+          children: [
+            loadProgress < 1
+                ? SizedBox(
+                    height: 7,
+                    child: LinearProgressIndicator(
+                      backgroundColor: lBlue,
+                      value: loadProgress,
+                    ))
+                : const SizedBox(),
+            Expanded(
+              child: error
+                  ? const PaginationText(text: "An error occurred")
+                  : WebView(
+                      initialUrl: widget.url,
+                      javascriptMode: JavascriptMode.unrestricted,
+                      onWebViewCreated: (controller) {
+                        _controller.complete(controller);
+                      },
+                      onWebResourceError: (_) => setState(() {
+                        error = true;
+                      }),
+                      navigationDelegate: (NavigationRequest request) {
+                        if (request.url.contains(widget.url)) {
+                          // Don't allow user to navigate to other pages
+                          return NavigationDecision.navigate;
+                        }
+                        return NavigationDecision.prevent;
+                      },
+                      onProgress: (progress) => setState(() {
+                        loadProgress = progress / 100;
+                      }),
+                    ),
+            ),
+          ],
         ),
       ),
     );
