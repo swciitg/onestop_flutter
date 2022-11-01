59daf7ec9d6f8be894f0e97bd05e17617eca8987
Hareesh nandigrama <85237630+Hareesh-Nandigrama@users.noreply.github.com>
2022-11-01 07:51:39 +0530

    Made necessary changes and corrected a typo in ip settings

--- lib/pages/ip/ip_carousel.dart
+++ lib/pages/ip/ip_carousel.dart
@@ -12,7 +12,7 @@ List<String> textdata = [
   "Click on 'Manage wireless networks'",
   'Right click on \'Local area connection\' and then click on properties',
   'Uncheck \'Internet Protocol Version 6 (TCP/IPv6)\' and double click \'Internet Protocol Version 4 (TCP/IPv4)\'',
-  'Select \'Use the following IP address\' and \'Use the following DNS server addresses\' Modify the DNS address as given below\nprimary DNS: 172.171.1.1\nsecondary DNS: 172.17.1.2',
+  'Select \'Use the following IP address\' and \'Use the following DNS server addresses\' Modify the DNS address as given below\nprimary DNS: 172.17.1.1\nsecondary DNS: 172.17.1.2',
   'Enter the following details to get your IP address',
   '',
   "Make sure the connection is no-proxy/direct connection. Open any website in your browser. It will show a captive portal asking your IITG login credentials.Login to the portal and start accessing internet using the same.If you have a problem while redirecting to login page, then use this link given below in your pc browser https://agnigarh.iitg.ac.in:1442/login?",
--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -30,90 +30,105 @@ class _ProfilePageState extends State<ProfilePage> {
     return Scaffold(
       appBar: appBar(context, displayIcon: false),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              //mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SizedBox(
-                  height: 16,
-                  width: MediaQuery.of(context).size.width,
-                ),
-                Text(
-                  "${context.read<LoginStore>().userData['name']}",
-                  textAlign: TextAlign.center,
-                  style: MyFonts.w800.setColor(kWhite).size(20),
-                ),
-                Text(
-                  '${context.read<LoginStore>().userData['rollno']}',
-                  textAlign: TextAlign.center,
-                  style: MyFonts.w500.setColor(kWhite).size(20),
-                ),
-                const HostelSelector(),
-                const SizedBox(
-                  height: 26,
-                ),
-                // Stack(
-                //   children: [
-                //     Container(
-                //       height: 300,
-                //       width: double.infinity,
-                //       color: kWhite,
-                //     ),
-                //     Positioned.fill(
-                //       child: Align(
-                //         alignment: Alignment.center,
-                //         child: Padding(
-                //           padding: const EdgeInsets.symmetric(horizontal: 16),
-                //           child: BarcodeWidget(
-                //             barcode: Barcode.code128(),
-                //             data: "${context.read<LoginStore>().userData['rollno']}",
-                //             height: 150,
-                //             color: kBlack,
-                //             backgroundColor: kWhite,
-                //             drawText: false,
-                //           ),
-                //         ),
-                //       ),
-                //     ),
-                //   ],
-                // ),
-                // Padding(
-                //   padding: const EdgeInsets.only(top: 20),
-                //   child: Center(
-                //     child: Text(
-                //       "Library issuing barcode",
-                //       textAlign: TextAlign.center,
-                //       style: MyFonts.w500.setColor(kWhite).size(18),
-                //     ),
-                //   ),
-                // ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: ElevatedButton(
-                      onPressed: () {
-                        context.read<LoginStore>().logOut(() =>
-                            Navigator.of(context).pushNamedAndRemoveUntil(
-                                '/', (Route<dynamic> route) => false));
-                      },
-                      style: ElevatedButton.styleFrom(
-                          elevation: 0, primary: kAppBarGrey),
-                      child: Text(
-                        'Log Out',
-                        style: MyFonts.w500.setColor(kBlue),
-                      )),
-                ),
-                TextButton(onPressed: (){
-                  showModalBottomSheet(context: context, builder: (BuildContext context){
-                    return FeedBack();
-                  });
-                }, child: Text('Want to report a bug or request a new feature?'))
-
-              ],
-            ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                height: 16,
+                width: MediaQuery.of(context).size.width,
+              ),
+              Text(
+                "${context.read<LoginStore>().userData['name']}",
+                textAlign: TextAlign.center,
+                style: MyFonts.w800.setColor(kWhite).size(20),
+              ),
+              Text(
+                '${context.read<LoginStore>().userData['rollno']}',
+                textAlign: TextAlign.center,
+                style: MyFonts.w500.setColor(kWhite).size(20),
+              ),
+              const HostelSelector(),
+              const SizedBox(
+                height: 26,
+              ),
+              // Stack(
+              //   children: [
+              //     Container(
+              //       height: 300,
+              //       width: double.infinity,
+              //       color: kWhite,
+              //     ),
+              //     Positioned.fill(
+              //       child: Align(
+              //         alignment: Alignment.center,
+              //         child: Padding(
+              //           padding: const EdgeInsets.symmetric(horizontal: 16),
+              //           child: BarcodeWidget(
+              //             barcode: Barcode.code128(),
+              //             data: "${context.read<LoginStore>().userData['rollno']}",
+              //             height: 150,
+              //             color: kBlack,
+              //             backgroundColor: kWhite,
+              //             drawText: false,
+              //           ),
+              //         ),
+              //       ),
+              //     ),
+              //   ],
+              // ),
+              // Padding(
+              //   padding: const EdgeInsets.only(top: 20),
+              //   child: Center(
+              //     child: Text(
+              //       "Library issuing barcode",
+              //       textAlign: TextAlign.center,
+              //       style: MyFonts.w500.setColor(kWhite).size(18),
+              //     ),
+              //   ),
+              // ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ElevatedButton(
+                    onPressed: () {
+                      context.read<LoginStore>().logOut(() =>
+                          Navigator.of(context).pushNamedAndRemoveUntil(
+                              '/', (Route<dynamic> route) => false));
+                    },
+                    style: ElevatedButton.styleFrom(
+                        elevation: 0, primary: kAppBarGrey),
+                    child: Text(
+                      'Log Out',
+                      style: MyFonts.w500.setColor(kBlue),
+                    )),
+              ),
+              Expanded(child: Container(),),
+              TextButton(
+                  onPressed: () {
+                    showModalBottomSheet(
+                        context: context,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(20),
+                          ),
+                        ),
+                        clipBehavior: Clip.antiAliasWithSaveLayer,
+                        isScrollControlled: true,
+                        builder: (BuildContext context) {
+                          return const FeedBack();
+                        });
+                  },
+                  child: Text(
+                    'Want to report a bug or request a new feature?',
+                    style: MyFonts.w500.size(13),
+                  )),
+              SizedBox(
+                height: 15,
+              ),
+            ],
           ),
         ),
       ),
