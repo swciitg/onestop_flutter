a3391f43cbee60b766471125477604dd8e984124
Chanchal Yadav <84243956+chanchalyadav272@users.noreply.github.com>
2023-06-16 02:54:36 +0530

    appbar on webview

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -1,11 +1,15 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:onestop_dev/globals/size_config.dart';
 import 'package:onestop_dev/pages/login/welcome.dart';
 import 'package:onestop_dev/widgets/login/login_webview.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+
+import '../../globals/my_colors.dart';
+import '../../globals/my_fonts.dart';
 
 class LoginPage extends StatefulWidget {
   static String id = "/login";
@@ -30,6 +34,74 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
+        appBar: loading
+            ? AppBar(
+                backgroundColor: kBackground,
+                iconTheme: const IconThemeData(color: kAppBarGrey),
+                automaticallyImplyLeading: false,
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: kAppBarGrey,
+                      child: IconButton(
+                        icon: const Icon(
+                          FluentIcons.arrow_left_24_regular,
+                          color: lBlue2,
+                        ),
+                        onPressed: () {
+                               Navigator.of(context)
+            .pushNamedAndRemoveUntil('/login', (Route<dynamic> route) => false);
+                        },
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(children: [
+                        WidgetSpan(
+                          child: Row(
+                            children: [
+                              Text(
+                                "One",
+                                textAlign: TextAlign.center,
+                                style: MyFonts.w600
+                                    .size(23)
+                                    .letterSpace(1.0)
+                                    .setColor(lBlue2),
+                              ),
+                              Text(
+                                ".",
+                                textAlign: TextAlign.center,
+                                style: MyFonts.w500.size(23).setColor(kYellow),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ]),
+                    ),
+                     Opacity(
+                      opacity: 0,
+                       child: CircleAvatar(
+                        backgroundColor: kAppBarGrey,
+                        child: IconButton(
+                          icon: const Icon(
+                            FluentIcons.arrow_left_24_regular,
+                            color: lBlue2,
+                          ),
+                          onPressed: () {
+                            // setState(() {
+                            //   loading=false;
+                            // });
+                          },
+                        ),
+                                         ),
+                     ),
+
+                  ],
+                ),
+                elevation: 0.0,
+              )
+            : null,
         body: loading
             ? SafeArea(child: LoginWebView(controller: _controller))
             : WelcomePage(setLoading: () {
