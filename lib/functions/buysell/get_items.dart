94cca7952b9d7e2692fe13c7e4154f71daed1bc2
Hareesh-Nandigrama <hareeshnandigrama@gmail.com>
2023-08-16 23:15:37 +0530

    removed unnecessary files

--- lib/functions/buysell/get_items.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:onestop_dev/services/api.dart';
-//
-// Future<List> getBuySellItems(mail) async {
-//   var list1 = await APIService().getBuyItems();
-//   var list2 = await APIService.getSellItems();
-//   var list3 = await APIService.getBnsMyItems(mail);
-//   return [list1, list2, list3];
-// }
