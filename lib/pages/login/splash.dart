63f3eead0b9d7471e3f3ffdc5fe701d5a5a314a6
Hareesh-Nandigrama <hareeshnandigrama@gmail.com>
2023-08-19 21:37:21 +0530

    removed async call from splash

--- lib/pages/login/splash.dart
+++ lib/pages/login/splash.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:onestop_dev/globals/enums.dart';
+import 'package:onestop_dev/globals/my_colors.dart';
+import 'package:onestop_dev/pages/home/home.dart';
 import 'package:onestop_dev/pages/login/blocked.dart';
+import 'package:onestop_dev/pages/login/login.dart';
 import 'package:onestop_dev/stores/login_store.dart';
+import 'package:shimmer/shimmer.dart';
 
 class SplashPage extends StatefulWidget {
   static String id = "/";
@@ -16,24 +20,34 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    LoginStore()
-        .isAlreadyAuthenticated()
-        .then((result) {
-      if (result == SplashResponse.authenticated && LoginStore.isProfileComplete){
-        Navigator.of(context)
-            .pushNamedAndRemoveUntil('/home2', (Route<dynamic> route) => false);
-      } else if(result == SplashResponse.blocked){
-        Navigator.of(context).pushNamedAndRemoveUntil(BlockedPage.id, (Route<dynamic> route) => false);
-      }else{
-        Navigator.of(context)
-            .pushNamedAndRemoveUntil('/login2', (Route<dynamic> route) => false);
-      }
-
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  FutureBuilder<SplashResponse>(
+        future: LoginStore().isAlreadyAuthenticated(),
+          builder: (context, snapshot){
+          if(snapshot.hasData)
+            {
+              SplashResponse result = snapshot.data!;
+              if (result == SplashResponse.authenticated && LoginStore.isProfileComplete){
+                return const HomePage();
+              } else if(result == SplashResponse.blocked){
+                return const BlockedPage();
+              }else{
+                return const LoginPage();
+              }
+            }
+          else if(snapshot.hasError)
+            {
+              return const Scaffold();
+            }
+          else
+            {
+              //Waiting State
+              return Scaffold(
+              );
+            }
+      });
   }
 }
