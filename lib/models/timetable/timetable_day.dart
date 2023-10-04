26399225fbd9fadcedd5d38b853b4d9138845c28
venkylm10 <venkylm10@gmail.com>
2023-10-04 17:38:15 +0530

    updated course model

--- lib/models/timetable/timetable_day.dart
+++ lib/models/timetable/timetable_day.dart
@@ -3,6 +3,21 @@ import 'package:onestop_dev/models/timetable/course_model.dart';
 class TimetableDay {
   List<CourseModel> morning = [];
   List<CourseModel> afternoon = [];
+  static const List<String> morningClasses = [
+    "8:00-9:00 AM",
+    "9:00-10:00 AM",
+    "10:00-11:00 AM",
+    "11:00-12:00 PM",
+    "12:00-1:00 PM",
+  ];
+
+  static const List<String> afternoonClasses = [
+    "1:00-2:00 PM",
+    "2:00-3:00 PM",
+    "3:00-4:00 PM",
+    "4:00-5-00 PM",
+    "5:00-6:00 PM",
+  ];
 
   void addMorning(CourseModel c) {
     // Tutorials first
