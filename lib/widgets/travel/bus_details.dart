582c5d302d740620dcaeaf731f641fc620c3af28
TUSHAR MISHRA <109910958+MISHRA-TUSHAR@users.noreply.github.com>
2023-10-13 19:11:26 +0530

    Update bus_details.dart
    Extracted the common logic for building the ListView for bus timings to a method called buildTimingList.
    Removed some unused code such as the initial variable declarations in _BusDetailsState and removed the initState since it was empty.
    Simplified the GestureDetector widgets for both city and campus.
    Used the new buildTimingList function in the FutureBuilder for both city and campus bus timings.

--- lib/widgets/travel/bus_details.dart
+++ lib/widgets/travel/bus_details.dart
@@ -11,8 +11,7 @@ import 'package:onestop_dev/widgets/ui/list_shimmer.dart';
 import 'package:provider/provider.dart';
 
 class BusDetails extends StatefulWidget {
-  late final int index;
-  // ignore: prefer_const_constructors_in_immutables
+  final int index;
   BusDetails({Key? key, required this.index}) : super(key: key);
 
   @override
@@ -22,149 +21,130 @@ class BusDetails extends StatefulWidget {
 class _BusDetailsState extends State<BusDetails> {
   bool isCity = false;
   bool isCampus = false;
-  List<TravelTiming>? busTime = [];
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     var store = context.read<TravelStore>();
     var daytype = store.busDayType;
+
+    Widget buildTimingList(List<TravelTiming> busTimings, String dayType, bool isFromCampus) {
+      return ListView.builder(
+        shrinkWrap: true,
+        physics: const ClampingScrollPhysics(),
+        itemCount: busTimings.length,
+        itemBuilder: (context, idx) {
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: Text(
+                  isFromCampus ? "To ${busTimings[idx].stop}" : "From ${busTimings[idx].stop}",
+                  style: MyFonts.w500.setColor(kWhite),
+                ),
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+                itemCount: isFromCampus
+                    ? (dayType == 'Weekdays' 
+                        ? busTimings[idx].weekdays.fromCampus.length
+                        : busTimings[idx].weekend.fromCampus.length)
+                    : (dayType == 'Weekdays'
+                        ? busTimings[idx].weekdays.toCampus.length
+                        : busTimings[idx].weekend.toCampus.length),
+                itemBuilder: (BuildContext context, int index) {
+                  final time = isFromCampus
+                      ? (dayType == 'Weekdays'
+                          ? formatTime(busTimings[idx].weekdays.fromCampus[index])
+                          : formatTime(busTimings[idx].weekend.fromCampus[index]))
+                      : (dayType == 'Weekdays'
+                          ? formatTime(busTimings[idx].weekdays.toCampus[index])
+                          : formatTime(busTimings[idx].weekend.toCampus[index]));
+                  final isLeft = dayType == 'Weekdays'
+                      ? hasLeft(isFromCampus
+                          ? busTimings[idx].weekdays.fromCampus[index]
+                          : busTimings[idx].weekdays.toCampus[index])
+                      : hasLeft(isFromCampus
+                          ? busTimings[idx].weekend.fromCampus[index]
+                          : busTimings[idx].weekend.toCampus[index]);
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: TimingTile(
+                      time: time,
+                      isLeft: isLeft,
+                      icon: FluentIcons.vehicle_bus_24_filled,
+                    ),
+                  );
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
     return FutureBuilder<List<TravelTiming>>(
-        future: store.getBusTimings(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            busTime = snapshot.data;
-            return Column(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      isCity = !isCity;
-                      isCampus = false;
-                    });
-                  },
-                  child: ListTile(
-                    title: Text(
-                      'Campus -> City',
-                      style: MyFonts.w500.setColor(kWhite),
-                    ),
-                    subtitle: Text(
-                      'Starting from Biotech park',
-                      style: MyFonts.w500.setColor(Colors.grey),
-                    ),
-                    trailing: Icon(
-                      isCity
-                          ? FluentIcons.chevron_up_24_regular
-                          : FluentIcons.chevron_down_24_regular,
-                      color: Colors.white,
-                    ),
+      future: store.getBusTimings(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final busTime = snapshot.data!;
+          return Column(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isCity = !isCity;
+                    isCampus = false;
+                  });
+                },
+                child: ListTile(
+                  title: Text(
+                    'Campus -> City',
+                    style: MyFonts.w500.setColor(kWhite),
+                  ),
+                  subtitle: Text(
+                    'Starting from Biotech park',
+                    style: MyFonts.w500.setColor(Colors.grey),
+                  ),
+                  trailing: Icon(
+                    isCity
+                        ? FluentIcons.chevron_up_24_regular
+                        : FluentIcons.chevron_down_24_regular,
+                    color: Colors.white,
                   ),
                 ),
-                isCity
-                    ?ListView.builder(
-                    shrinkWrap: true,
-                    physics: const ClampingScrollPhysics(),
-                    itemCount:busTime!.length,
-                      itemBuilder: (context, int idx) {
-                        return Column(
-                  children: [
-                        Padding(
-                          padding:
-                          const EdgeInsets.symmetric(vertical: 5),
-                          child: Text(
-                            "To ${busTime![idx].stop}",
-                            style: MyFonts.w500.setColor(kWhite),
-                          ),
-                        ),
-                        ListView.builder(
-                            shrinkWrap: true,
-                            physics: const ClampingScrollPhysics(),
-                            itemCount:daytype == 'Weekdays' ?busTime![idx].weekdays.fromCampus.length:busTime![idx].weekend.fromCampus.length,
-                            itemBuilder:(BuildContext context, int index){
-                              return Padding(
-                                padding: const EdgeInsets.symmetric(vertical: 5),
-                                child: TimingTile(
-                                  time:  daytype == 'Weekdays' ? formatTime(busTime![idx].weekdays.fromCampus[index]):formatTime(busTime![idx].weekend.fromCampus[index]),
-                                  isLeft: daytype=='Weekdays'?   hasLeft(busTime![idx].weekdays.fromCampus[index]):hasLeft(busTime![idx].weekend.fromCampus[index]),
-                                  icon: FluentIcons.vehicle_bus_24_filled,
-
-                                ),
-                              );
-                            }
-                        ),
-                  ],
-                );
-                      }
-                    )
-                    : Container(),
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      isCampus = !isCampus;
-                      isCity = false;
-                    });
-                  },
-                  child: ListTile(
-                    title: Text(
-                      'City -> Campus',
-                      style: MyFonts.w500.setColor(kWhite),
-                    ),
-                    subtitle: Text(
-                      'Starting from City',
-                      style: MyFonts.w500.setColor(Colors.grey),
-                    ),
-                    trailing: Icon(
-                      isCampus
-                          ? FluentIcons.chevron_up_24_regular
-                          : FluentIcons.chevron_down_24_regular,
-                      color: Colors.white,
-                    ),
+              ),
+              isCity ? buildTimingList(busTime, daytype, true) : Container(),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isCampus = !isCampus;
+                    isCity = false;
+                  });
+                },
+                child: ListTile(
+                  title: Text(
+                    'City -> Campus',
+                    style: MyFonts.w500.setColor(kWhite),
+                  ),
+                  subtitle: Text(
+                    'Starting from City',
+                    style: MyFonts.w500.setColor(Colors.grey),
+                  ),
+                  trailing: Icon(
+                    isCampus
+                        ? FluentIcons.chevron_up_24_regular
+                        : FluentIcons.chevron_down_24_regular,
+                    color: Colors.white,
                   ),
                 ),
-                isCampus
-                    ? ListView.builder(
-                    shrinkWrap: true,
-                    physics: const ClampingScrollPhysics(),
-                    itemCount:busTime!.length,
-                      itemBuilder: (context, idx) {
-                        return Column(
-                  children: [
-                        Padding(
-                          padding:
-                          const EdgeInsets.symmetric(vertical: 5),
-                          child: Text(
-                            "From ${busTime![idx].stop}",
-                            style: MyFonts.w500.setColor(kWhite),
-                          ),
-                        ),
-                        ListView.builder(
-                            shrinkWrap: true,
-                            physics: const ClampingScrollPhysics(),
-                            itemCount:daytype == 'Weekdays' ?busTime![idx].weekdays.toCampus.length : busTime![idx].weekend.toCampus.length,
-                            itemBuilder:(BuildContext context, int index){
-                              return Padding(
-                                padding: const EdgeInsets.symmetric(vertical: 5),
-                                child: TimingTile(
-                                  time:  daytype == 'Weekdays' ? formatTime(busTime![idx].weekdays.toCampus[index]):formatTime(busTime![idx].weekend.toCampus[index]),
-                                  isLeft: daytype=='Weekdays'?hasLeft(busTime![idx].weekdays.toCampus[index]):hasLeft(busTime![idx].weekend.toCampus[index]),
-                                  icon: FluentIcons.vehicle_bus_24_filled,
-
-                                ),
-                              );
-                            }
-                        ),
-                  ],
-                );
-                      }
-                    )
-                    : Container(),
-              ],
-            );
-          }
-          return ListShimmer(count: 2);
-        });
+              ),
+              isCampus ? buildTimingList(busTime, daytype, false) : Container(),
+            ],
+          );
+        }
+        return ListShimmer(count: 2);
+      },
+    );
   }
 }
