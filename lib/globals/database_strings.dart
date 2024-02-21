991cbf47251487ab833d5233961d140c7edb1c53
Hareesh-Nandigrama <85237630+Hareesh-Nandigrama@users.noreply.github.com>
2024-02-20 23:13:18 -0800

    Update database_strings.dart

--- lib/globals/database_strings.dart
+++ lib/globals/database_strings.dart
@@ -1,13 +1,13 @@
 abstract class DatabaseRecords {
   static const restaurant = "Restaurant";
-  static const timetable = "timetable";
+  static const timetable = "Timetable";
   static const lastUpdated = "LastUpdated";
   static const starredContacts = "StarredContacts";
   static const contacts = "Contact";
   static const messMenu = "MessMenu";
   static const ferryTimings = "ferryTimings";
   static const busTimings = "busTimings";
-  static const homePage = "homePage";
+  static const homePage = "HomePage";
 }
 
 class BackendHelper {
