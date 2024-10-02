37ec00c0ec8c24449f886585c291c61afa8f9fdf
ganeshsinnur <ganeshsinnur07@gmail.com>
2024-10-02 12:59:07 +0530

    minor updates done

--- lib/pages/medical_section/medicalmenu/feedback/pharmacy_feedback.dart
+++ lib/pages/medical_section/medicalmenu/feedback/pharmacy_feedback.dart
@@ -37,7 +37,7 @@ class _PharmacyFeedbackState extends State<PharmacyFeedback> {
   @override
   void initState() {
     super.initState();
-    patientName.text = LoginStore.userData['name'] ?? "";
+    patientName.text = LoginStore.isGuest?"": LoginStore.userData['name'] ?? "";
     mobilenumber.text = LoginStore.userData['phoneNumber'].toString();
     _datecontroller.text = DateFormat('dd-MMM-yyyy')
         .format(DateTime.parse(DateTime.now().toIso8601String()));
--- lib/pages/medical_section/medicalmenu/medical_reimbursement.dart
+++ lib/pages/medical_section/medicalmenu/medical_reimbursement.dart
@@ -1,3 +1,4 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:onestop_kit/onestop_kit.dart';
@@ -165,12 +166,14 @@ class MedicalReimbursement extends StatelessWidget {
                     children: [
                       const Icon(Icons.info_outline_rounded, color: Colors.grey,size: 24,),
                       const SizedBox(width: 8),
+                      Spacer(),
                       Expanded(
                         child: Text(
                           "For any Hospitalization (not covered under insurance), please submit Form 3",
                           style: MyFonts.w700.setColor(kWhite).size(14).copyWith(fontWeight: FontWeight.w400,color: Colors.white),
                           softWrap: true,
-                          overflow: TextOverflow.visible
+                          overflow: TextOverflow.visible,
+                          //textAlign: BadgePosition.bottomStart(bottom: -8),
                         ),
 
                       ),
