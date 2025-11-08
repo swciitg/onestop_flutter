e41766a034f327b87848e2e0142e615f55fdd715
Kashish Shakya <kashishshakya2020@gmail.com>
2025-11-08 12:56:19 +0530

    Update existing flutter ui

--- lib/pages/profile/profile_page.dart
+++ lib/pages/profile/profile_page.dart
@@ -1,11 +1,14 @@
+// import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:onestop_kit/onestop_kit.dart';
-
-import '../../globals/my_colors.dart';
+import 'package:onestop_ui/index.dart';
+import '../../globals/my_fonts.dart';
 import '../../stores/login_store.dart';
-import '../../widgets/profile/data_tile.dart';
-import 'edit_profile.dart';
+import 'package:barcode_widget/barcode_widget.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
 
 class ProfilePage extends StatefulWidget {
   final OneStopUser profileModel;
@@ -17,31 +20,56 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+
+  late OneStopUser _user; 
+
+  @override
+  void initState() {
+    super.initState();
+    _user = widget.profileModel; 
+  }
+  
+  void handleFieldUpdate(String label, String newValue) async{
+  setState(() {
+    switch (label) {
+      case 'Alternate Email':
+        _user = _user.copyWith(altEmail: newValue);
+        break;
+      case 'Cycle Registration Number':
+        _user = _user.copyWith(cycleReg: newValue);
+        break;
+      case 'Linkedin Profile':
+        _user = _user.copyWith(linkedin: newValue);
+        break;
+    }
+  });
+   final prefs = await SharedPreferences.getInstance();
+  await prefs.setString('userInfo', jsonEncode(_user.toJson()));
+
+  LoginStore.userData = _user.toJson();
+}
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kBackground,
+      backgroundColor: OColor.gray100,
       appBar: AppBar(
-        backgroundColor: kAppBarGrey,
-        iconTheme: const IconThemeData(color: kAppBarGrey),
+        backgroundColor: OColor.gray100,
+        iconTheme:  IconThemeData(color: OColor.green600),
         automaticallyImplyLeading: false,
-        centerTitle: true,
-        // leadingWidth: 16,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          icon: const Icon(
-            Icons.arrow_back_ios_new_outlined,
-            color: kWhite,
-          ),
-          iconSize: 20,
-        ),
         title: Text(
           "Profile",
           textAlign: TextAlign.left,
-          style: OnestopFonts.w500.size(23).setColor(kWhite),
+          style: OTextStyle.headingLarge.copyWith(color: OColor.gray800)
         ),
+      actions: [
+        IconButton(
+      icon: const Icon(Icons.notifications_outlined),
+      onPressed: () {
+      },
+    ),
+      ],
       ),
       body: SafeArea(
         child: SingleChildScrollView(
@@ -52,116 +80,358 @@ class _ProfilePageState extends State<ProfilePage> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const SizedBox(
-                height: 12,
+                height: OSpacing.s,
               ),
-              // Center(
-              //     child: Stack(alignment: Alignment.bottomRight, children: [
-              //   ClipRRect(
-              //       borderRadius: BorderRadius.circular(75.0),
-              //       child: Image(
-              //         image: widget.profileModel?.image == null
-              //             ? const ResizeImage(
-              //                 AssetImage(
-              //                     'assets/images/profile_placeholder.png'),
-              //                 width: 150,
-              //                 height: 150)
-              //             : ResizeImage(
-              //                 MemoryImage(
-              //                     base64Decode(widget.profileModel!.image!)),
-              //                 width: 150,
-              //                 height: 150,
-              //               ),
-              //         fit: BoxFit.fill,
-              //       )),
-              // ])),
-              // const SizedBox(
-              //   height: 24,
-              // ),
-              Text('Basic Information',
-                  style: OnestopFonts.w600.size(16).setColor(kWhite)),
+              Container(
+                width: 400,
+                padding: const EdgeInsets.all(OSpacing.m),
+                decoration: BoxDecoration(
+                  color: OColor.white,
+                  borderRadius: const BorderRadius.all(Radius.circular(OCornerRadius.l)),
+                  border: Border.all(color: OColor.green600)
+                ),
+                child: Column( 
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Column(
+                      children: [
+                       Row(  mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                           Image.asset('assets/images/iitg_logo.png', height: 40,),
+                           SizedBox(width: 16,),
+                           Column( crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Indian Institute of', style: OTextStyle.headingXSmall.copyWith(color: OColor.gray800),),
+                              Text('Technology, Guwahati', style: OTextStyle.headingXSmall.copyWith(color: OColor.gray800),)
+                            ],
+                           )
+                        ],
+                       ),
+                       const SizedBox(height: OSpacing.s),
+                          Divider(thickness: 1, color: OColor.gray200,),
+                        const SizedBox(height: OSpacing.s),
+
+                        const CircleAvatar(
+                        radius: 45,
+                        backgroundImage: AssetImage('assets/images/profile_placeholder.jpg'),
+                        ),
+                        const SizedBox(height: OSpacing.s),
+                           Text(_user.name,
+                          style: OTextStyle.headingMedium.copyWith(color: OColor.gray800)
+                         ),
+                        const SizedBox(height: OSpacing.xxs),
+                         Text(_user.rollNo,
+                          style: OTextStyle.headingXSmall.copyWith(color: OColor.gray800)
+                         ),
+                        const SizedBox(height: OSpacing.xxs),
+                         Text('CSE Department',
+                          style: OTextStyle.headingXSmall.copyWith(color: OColor.gray800)
+                         ),
+                        const SizedBox(height: OSpacing.m),
+                          BarcodeWidget(
+                        barcode: Barcode.code128(),
+                        data: _user.rollNo, 
+                         width: 200,
+                        height: 60,
+                        drawText: false,
+                        color: OColor.black, 
+                       ),
+
+                       const SizedBox(height: 10),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 28,),
+              Text('Additional Information',
+                  style: MyFonts.w500.copyWith(color: OColor.gray800)),
               const SizedBox(
-                height: 6,
+                height: OSpacing.m,
               ),
-              DataTile(
-                title: 'Username',
-                semiTitle: widget.profileModel.name,
-              ),
-              DataTile(
-                title: 'Roll Number',
-                semiTitle: widget.profileModel.rollNo,
-              ),
-              DataTile(
-                title: 'Outlook ID',
-                semiTitle: widget.profileModel.outlookEmail,
-              ),
-              DataTile(
-                title: 'Alt Email',
-                semiTitle: widget.profileModel.altEmail,
-              ),
-              DataTile(
-                title: 'Contact Number',
-                semiTitle: widget.profileModel.phoneNumber?.toString(),
-              ),
-              DataTile(
-                title: 'Emergency Contact Number',
-                semiTitle: widget.profileModel.emergencyPhoneNumber?.toString(),
-              ),
-              DataTile(
-                title: 'Hostel',
-                semiTitle: widget.profileModel.hostel
-                    ?.getHostelFromDatabaseString()
-                    ?.displayString,
-              ),
-              DataTile(
-                  title: 'Subscribed Mess',
-                  semiTitle: widget.profileModel.subscribedMess
+              
+                  Container(
+               width: 400,
+                padding: const EdgeInsets.all(OSpacing.m),
+               decoration: BoxDecoration(
+                 color: OColor.white,
+                  borderRadius: const BorderRadius.all(Radius.circular(OCornerRadius.l)),
+                  border: Border.all(color: OColor.green600)
+             ),
+                child: Column(
+                 children:  [
+                 InfoTile(
+                  icon: Icons.email_outlined,
+                  label: 'Outlook ID',
+                  value: _user.outlookEmail,
+            ),
+                  InfoTile(
+                   icon: Icons.email_outlined,
+                  label: 'Alternate Email',
+                  value: _user.altEmail ?? '',
+                 editable: true,
+                 onUpdated: handleFieldUpdate,
+            ),
+                   InfoTile(
+                    icon: Icons.phone_outlined,
+                  label: 'Contact Number',
+                  value: _user.phoneNumber?.toString() ?? '',
+             ),
+                InfoTile(
+                 icon: Icons.phone,
+                label: 'Emergency Contact Number',
+                value: _user.emergencyPhoneNumber?.toString() ?? '',
+          ),
+             InfoTile(
+            icon: Icons.home_outlined,
+            label: 'Hostel',
+            value: _user.hostel  ?.getHostelFromDatabaseString()
+                    ?.displayString ?? '',
+          ),
+          InfoTile(
+            icon: Icons.restaurant_outlined,
+            label: 'Subscribed Mess',
+            value: _user.subscribedMess
                       ?.getMessFromDatabaseString()
-                      ?.displayString),
-              widget.profileModel.dob == null
-                  ? Container()
-                  : DataTile(
-                      title: 'Date of Birth',
-                      semiTitle: DateFormat('dd-MMM-yyyy')
-                          .format(DateTime.parse(widget.profileModel.dob!)),
-                    ),
-              DataTile(
-                title: 'Home Address',
-                semiTitle: widget.profileModel.homeAddress,
-              ),
-              DataTile(
-                title: 'Cycle Registration Number',
-                semiTitle: widget.profileModel.cycleReg,
-              ),
-              DataTile(
-                title: 'LinkedIn Profile',
-                semiTitle: widget.profileModel.linkedin,
-              ),
+                      ?.displayString?? '',
+          ),
+          InfoTile(
+            icon: Icons.calendar_today_outlined,
+            label: 'Date of Birth',
+            value: DateFormat('dd-MM-yyyy')
+                          .format(DateTime.parse(_user.dob!)),
+                    
+          ),
+          InfoTile(
+            icon: Icons.home_outlined,
+            label: 'Home Address',
+            value: _user.homeAddress ??'',
+          ),
+          InfoTile(
+            icon: Icons.pedal_bike_outlined,
+            label: 'Cycle Registration Number',
+            value: _user.cycleReg ?? '',
+            editable: true,
+            onUpdated: handleFieldUpdate,
+          ),
+          InfoTile(
+            icon: Icons.link_outlined,
+            label: 'Linkedin Profile',
+            value: _user.linkedin ?? '',
+            editable: true,
+            onUpdated: handleFieldUpdate,
+            showdivider: false,
+          ),
+        ],
+      )
+                  ),
               const SizedBox(
-                height: 24,
+                height: OSpacing.l,
               ),
+              SizedBox(
+               width: double.infinity,
+                child: OutlinedButton.icon(
+                 onPressed: () {
+                     LoginStore().logOut(() => Navigator.of(context)
+                          .pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false));
+               },
+              icon:  Icon(Icons.logout_outlined, color: OColor.red600),
+               label:  Text(
+                  "Log Out",
+                style: OTextStyle.labelMedium.copyWith(color: OColor.red600)
+           ),
+                 style: OutlinedButton.styleFrom(
+                 side:  BorderSide(color: OColor.gray200, width: 1),
+                 shape: RoundedRectangleBorder(
+                 borderRadius: BorderRadius.circular(16),
+            ),
+                 padding: const EdgeInsets.symmetric(vertical: 12),
+                 backgroundColor: OColor.white,
+          ),
+         ),
+        ),
+                    SizedBox(height: 50,),
+
             ],
           ),
         )),
       ),
-      floatingActionButton: LoginStore.isGuest
-          ? Container()
-          : GestureDetector(
-              onTap: (() {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => EditProfile(
-                          profileModel: widget.profileModel,
-                        )));
-              }),
-              child: Container(
-                width: 48,
-                height: 48,
-                alignment: Alignment.center,
-                decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(16)),
-                    color: lBlue2),
-                child: const Icon(Icons.edit_outlined),
-              ),
-            ),
     );
   }
 }
+
+class InfoTile extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final String value;
+  final bool editable;
+  final bool showdivider;
+  final Function(String label, String newValue)? onUpdated;
+
+const InfoTile({
+  super.key,
+  required this.icon,
+  required this.label,
+  required this.value,
+  this.editable = false,
+  this.showdivider = true,
+  this.onUpdated,
+});
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Icon(icon, color: OColor.green600, size: 16),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    label,
+                    style: OTextStyle.headingXSmall.copyWith(color: OColor.gray600)
+                  ),
+                  const SizedBox(height: OSpacing.xxs),
+                  Text(
+                    value,
+                    style: OTextStyle.headingXSmall.copyWith(color: OColor.gray800)
+                  ),
+                ],
+              ),
+            ),
+           if (editable)
+  IconButton(
+    onPressed: () {
+      showEditSheet(
+        context,
+        icon,
+        label,
+        value,
+        (newValue) {
+          onUpdated?.call(label, newValue);
+        },
+      );
+    },
+    icon: Icon(Icons.edit_outlined, color: OColor.green600, size: 16),
+  ),
+
+          ],
+        ),
+         const SizedBox(height: 10),
+        if (showdivider) ...[
+         
+          Divider(color: OColor.gray200, thickness: 1),
+          const SizedBox(height: 10),
+        ],
+      ],
+    );
+  }
+}
+
+void showEditSheet(
+  BuildContext context,
+  IconData icon,
+  String label,
+  String currentValue,
+  Function(String newValue) onSave,
+) {
+  final TextEditingController controller =
+      TextEditingController(text: currentValue);
+
+  showModalBottomSheet(
+    context: context,
+    isScrollControlled: true,
+    backgroundColor: Colors.transparent,
+    builder: (context) {
+      return Padding(
+        padding: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height*0.4,
+          decoration: BoxDecoration(
+            color: OColor.white,
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(32)),
+          ),
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                   Icon(icon, size: 24, color: OColor.gray800,),
+                  const SizedBox(width: OSpacing.xs),
+                 Expanded(child:  Text(
+                    "Edit $label",
+                    style: OTextStyle.labelLarge.copyWith(color: OColor.gray800),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),),
+                 
+                  IconButton(
+                    icon:  Icon(Icons.close, color: OColor.gray600,),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "Enter $label",
+                style:  OTextStyle.labelMedium.copyWith(color: OColor.gray800)
+              ),
+              const SizedBox(height: OSpacing.xs),
+              TextField(
+                controller: controller,
+                style: OTextStyle.bodySmall.copyWith(color: OColor.gray600),
+                decoration: InputDecoration(
+                  hintText: "Enter $label...",hintStyle: OTextStyle.bodySmall.copyWith(color: OColor.gray600),
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  border:
+                      OutlineInputBorder(borderRadius: BorderRadius.circular(OCornerRadius.m), borderSide: BorderSide(
+                        color: OColor.gray200,
+                        width: 1.0
+                      )),
+                    
+                
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                height: 48,
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    onSave(controller.text.trim());
+                  },
+                  icon: Icon(Icons.check, color: OColor.white),
+                  label: Text(
+                    "Save Changes",
+                    style: OTextStyle.labelMedium.copyWith(color: OColor.white)
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: OColor.green600,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(OCornerRadius.m),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: OSpacing.xs),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
+
